870e1487f0a8a78003a0909f7f2b568682ddd972
Ashok-Dd <bonguashok86@gmail.com>
2025-02-04 08:11:54 +0530

    6th

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -3,23 +3,23 @@ const Head = (
 )
 
 const Body = (
-    <div className="w-[10px] h-[150px] sm:h-[100px] absolute right-0 top-[110px] bg-black "/>
+    <div className="w-[10px] h-[102px]  absolute right-0 top-[110px] bg-black" />
 )
 
 const RightArm = (
-    <div className="w-[100px] sm:w[70px] h-[10px] bg-black rotate-30 absolute top-[150px] right-[-90px] " />
+    <div className="w-[100px] sm:w-[70px] h-[10px] bg-black rotate-30 absolute top-[150px] right-[-60px] " />
 )
 
 const LeftArm = (
-    <div className="w-[100px] sm:w[70px] h-[10px] bg-black rotate-150 absolute top-[150px] right-0 " />
+    <div className="w-[100px] sm:w-[70px] h-[10px] bg-black rotate-150 absolute top-[150px] right-0 " />
 )
 
 const LeftLeg = (
-    <div className="w-[100px] sm:w[70px] h-[10px] bg-black rotate-30 absolute top-[275px] sm:top-[225px] right-[-90px] " />
+<div className="w-[100px] sm:w-[70px] h-[10px] bg-black rotate-30 absolute top-[225px] right-[-60px] transform" />
 )
 
 const RightLeg = (
-    <div className="w-[100px] sm:w[70px] h-[10px] bg-black rotate-150 absolute top-[275px] sm:top-[225px] right-0 " />
+    <div className="w-[100px] sm:w-[70px] h-[10px] bg-black rotate-150 absolute top-[225px] right-0 " />
 )
 
 
@@ -32,11 +32,11 @@ type HangManProps = {
 
 export default function HangmanDrawing({ numberOfGuesses } : HangManProps ) {
   return (
-    <div className="relative ">
+    <div className="relative  ">
         {BodyParts.slice(0 , numberOfGuesses)}
         <div className="h-[50px] w-[10px] absolute top-0 right-0 bg-black" />
 
-        <div className="h-[10px] w-[200px] sm:w-[150px]  ml-[120px] bg-black" />
+        <div className="h-[10px] w-full max-w-[120px]   ml-[120px] bg-black" />
 
         <div className="w-[10px]  sm:h-[300px] h-[350px]  bg-black ml-[120px] " />
 
